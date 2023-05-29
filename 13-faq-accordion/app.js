eaf0ec9e5134aa971b7432a44d894b13c234125d
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-05-29 07:08:32 +0300

    finish javascript logic

---
+++ 13-faq-accordion/app.js
@@ -0,0 +1,30 @@
+const faqAccordions = document.querySelectorAll('.faq__accordion');
+
+faqAccordions.forEach((faqAccordion) => {
+  faqAccordion.addEventListener('click', (e) => {
+    console.dir(e.currentTarget);
+    console.log(e.currentTarget.children[2]);
+    const angleDownIcon = e.currentTarget.children[0];
+    const accordionHeading = e.currentTarget.children[1];
+    const accordionContent = e.currentTarget.children[2];
+    const angleUpIcon = e.currentTarget.children[3];
+
+    if (accordionHeading.classList.contains('open')) {
+      accordionContent;
+    }
+
+    if (accordionHeading.classList.contains('open')) {
+      accordionHeading.classList.remove('open');
+      accordionContent.classList.remove('show');
+
+      angleDownIcon.style.display = 'block';
+      angleUpIcon.style.display = 'none';
+    } else {
+      accordionHeading.classList.add('open');
+      accordionContent.classList.add('show');
+
+      angleDownIcon.style.display = 'none';
+      angleUpIcon.style.display = 'block';
+    }
+  });
+});
