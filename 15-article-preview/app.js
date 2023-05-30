06d542c2a2ebf6c3c4df288f555cd2b8f085f2c9
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-05-30 12:21:43 +0300

    Javascript : add aniamtion upon clicking on share button

---
+++ 15-article-preview/app.js
@@ -0,0 +1,10 @@
+const shareIcon = document.querySelector('#link-share');
+const toolTip = document.querySelector('.tooltip');
+
+
+shareIcon.addEventListener('click', () => {
+  console.log('hey');
+  // toolTip.style.opacity = 1;
+  // toolTip.style.animation = 'animate-tooltip 1s linear;'
+  toolTip.classList.add('fadeInFromButtom')
+})
