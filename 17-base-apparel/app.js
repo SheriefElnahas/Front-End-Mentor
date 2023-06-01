3ef2ef92b953506f4cc9d4e62fc80e199d33074a
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-06-01 20:41:22 +0300

    finish desktop design

--- 17-base-apparel/app.js
+++ 17-base-apparel/app.js
@@ -6,14 +6,14 @@ const errorIconElement = document.querySelector('.error__icon');
 formElement.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  console.dir(e.target);
+
 })
 
 emailInput.addEventListener('input', (e) => {
-  console.dir(e.target)
+
   if (e.target.validationMessage.length === 0) {
-   errorMessage.style.visibility = 'hidden';
-   errorIconElement.style.visibility = 'hidden';
+    errorMessage.style.visibility = 'hidden';
+    errorIconElement.style.visibility = 'hidden';
   } else {
     errorMessage.style.visibility = 'visible';
     errorIconElement.style.visibility = 'visible';
